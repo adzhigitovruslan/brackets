9b0e022ec428f8053470659ee29106e26e74867a
adzhigitovruslan <adzhigitovruslan@gmail.com>
2023-02-05 19:55:10 +0800

    feat: implement js function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let OPEN_BRACKETS = [];
+  let PAIR_BRACKETS = {};
+  let stack = [];
+
+  bracketsConfig.forEach(el => {
+    OPEN_BRACKETS.push(el[0]);
+    PAIR_BRACKETS[el[1]] = el[0];
+  });
+
+  for(let i = 0; i < str.length; i++) {
+    let top = stack[stack.length - 1];
+    (stack.length !== 0) && PAIR_BRACKETS[str[i]] === top ? stack.pop() : stack.push(str[i])
+  }
+  return stack.length === 0;
 }
